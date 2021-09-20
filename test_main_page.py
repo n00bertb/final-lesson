cad9a647f7ce27e21f948524df3425908bdb101d
n00bertb <53352025+n00bertb@users.noreply.github.com>
2021-09-20 21:18:59 +0300

    добавил маркировку к тестам @pytest.mark.need_review
    добавил маркировку к тестам @pytest.mark.need_review

--- test_main_page.py
+++ test_main_page.py
@@ -22,7 +22,8 @@ class TestLoginFromMainPage():
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-        
+
+@pytest.mark.need_review        
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = BasketPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -30,7 +31,8 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()         # переходим в корзину
     page.check_emty_basket()        #проверяем корзину на пустоту
     #time.sleep(1000)
-      
+
+@pytest.mark.need_review      
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
--- test_product_page.py
+++ test_product_page.py
@@ -16,7 +16,8 @@ class TestUserAddToBasketFromProductPage():
         #time.sleep(100)
         page.should_be_authorized_user()
         #time.sleep(100)
-
+        
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -44,3 +45,17 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+ 
+@pytest.mark.need_review       
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link =  f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_to_basket()    #жмем кнопку добавить в карзину
+    page.solve_quiz_and_get_code() #решаем задачу и получаем ответ
+    #time.sleep(10)
+    page.should_be_same_product_name() #проверяем что добавился верный продукт в корзину
+    page.should_be_correct_basket_price() #проверяем что цена в корзине коректная   
