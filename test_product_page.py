85b4a253ba1492211154b663b02d1dd3cfccfa7d
n00bertb <53352025+n00bertb@users.noreply.github.com>
2021-09-20 21:28:50 +0300

    последние изменения промаркировал не тот тест, плюс перенес тесты в файл test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -40,6 +40,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -58,4 +59,13 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code() #решаем задачу и получаем ответ
     #time.sleep(10)
     page.should_be_same_product_name() #проверяем что добавился верный продукт в корзину
-    page.should_be_correct_basket_price() #проверяем что цена в корзине коректная   
+    page.should_be_correct_basket_price() #проверяем что цена в корзине коректная
+
+@pytest.mark.need_review      
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_basket_page()        # переходим в корзину
+    page.check_emty_basket()        #проверяем корзину на пустоту
+    #time.sleep(1000)
