5208b73987cb69287c66b5da4a98d790b52457b7
n00bertb <53352025+n00bertb@users.noreply.github.com>
2021-09-23 13:28:33 +0300

    добавил недостающие методы в product_page.py
    добавил методы для теста test_guest_cant_see_product_in_basket_opened_from_product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -33,3 +33,18 @@ class ProductPage(BasePage):
 
     def should_disappeared(self):
         assert self.is_disappeared(*BasketPageLocators.SUCCESS_MESSAGE), "Success message is not disappeared"
+        
+    def check_emty_basket(self):
+        self.should_be_emty_basket_text()
+        self.should_be_no_products_in_basket()
+        
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
+        basket_button.click()
+        
+    def should_be_emty_basket_text(self):
+        emty_text='Your basket is empty. Continue shopping'
+        assert emty_text == self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_TEXT).text, "Text not presented, basket not empty"
+    
+    def should_be_no_products_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.CHECKOUT_BUTTON), "Product is presented in basket, but should not be"   
